6aecf8f74084d14e6cf5a069149633b452f760c7
Christos Chro <92549742+Pythoner321123@users.noreply.github.com>
2022-05-23 20:24:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
 import os, shutil
+
+# Our lists
 dstFiles = []
 srcFiles = []
-src = r"D:\code\python\projects\testFolder\src"
-dst = r"D:\code\python\projects\testFolder\dst"
+#------------
+src = r"" # Provide the directory of your source folder inside of the quotation marks.
+dst = r"" # Create a folder and then provide the directory of your destination folder inside the quotation.
 
+
+# These loops bellow are listing every file from the 2 directories then appending them to a list
 def copy(source, destinatiun):
     for file in os.listdir(source):
         try:
             if os.path.isdir(os.path.join(source, file)):
                 shutil.copytree(os.path.join(source, file), os.path.join(destinatiun, file))
         except FileExistsError:
-            pass
+            print("Sorry, an error has occured. Pleaase try again.")
+        except PermissionError:
+            print("Sorry, an error has occured. Pleaase try again.")
         else:
             with open(f"{source}\\{file}", "rb") as f:
-                    with open(f"{destinatiun}\\{file}", "wb") as dstf:
-                        dstf.write(f.read())
-              
+                with open(f"{destinatiun}\\{file}", "wb") as dstf:
+                    dstf.write(f.read())
+
+
 for dstFile in os.listdir(dst):
     dstFiles.append(dstFile)
 for srcFile in os.listdir(src):
     srcFiles.append(srcFile)
 
+# These if statements are checking files and figuring out which directory is missing a file
+# if there's a file missing they're "copying" and pasting that file into the directory
 if len(srcFiles) != len(dstFiles):
-        for srcFile in srcFiles:
-            if srcFile not in dstFiles:
-                copy(src, dst)   
-        for dstFile in dstFiles:
-            if dstFile not in srcFiles:
-                copy(dst, src)
+    for srcFile in srcFiles:
+        if srcFile not in dstFiles:
+            copy(src, dst)
+    for dstFile in dstFiles:
+        if dstFile not in srcFiles:
+            copy(dst, src)
